0aa709a0905e6c17645f051e803109c2e7fe067f
2sty <atlustowski@gmail.com>
2020-11-13 17:09:42 +0100

    simply authentication

--- app.js
+++ app.js
@@ -19,7 +19,38 @@ const userSchema = {
 
 const User = new mongoose.model("User", userSchema);
 
+app.post("/register", function(req,res){
+  const newUser = new User({
+    email:req.body.username,
+    password:req.body.password
+  });
 
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  })
+});
+
+app.post("/login", function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+
+});
 
 app.get("/", function(req,res){
   res.render("home");
