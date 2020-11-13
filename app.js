679f30ff526da3d0ee34d355f29f5d2eb0bbc7dd
2sty <atlustowski@gmail.com>
2020-11-13 16:49:12 +0100

    add mongoose

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true,useUnifiedTopology:true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
 app.get("/", function(req,res){
   res.render("home");
 });
